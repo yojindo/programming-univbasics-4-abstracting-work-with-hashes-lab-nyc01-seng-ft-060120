05b413cf762c58909df9a20869603569ee17517e
Jake Kwak <jakejaekwak@gmail.com>
2020-05-12 19:42:51 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = {key => value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+hash[key]
 end
 
 def update_counting_hash(hash, key)
